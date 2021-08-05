2ba83a0a4ad79132495f516a199e9174735a8109
yutakiguchi <y--k080816@docomo.ne.jp>
2021-08-05 09:15:37 +0000

    お気に入り

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   attachment :profile_image
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
