dcdba696e5a4f628f8e5c16f85fd9fe64b5c0d1b
yutakiguchi <y--k080816@docomo.ne.jp>
2021-08-07 10:34:52 +0000

    コメント非同期

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,20 +1,19 @@
 class BookCommentsController < ApplicationController
-  
+
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    @comment.save
   end
-  
+
   def destroy
-    BookComment.find_by(id: params[:id],book_id: params[:book_id]).destroy
-    redirect_back(fallback_location: root_path)
+    @comment = BookComment.find_by(id: params[:id],book_id: params[:book_id])
+    @comment.destroy
   end
-  
+
   private
-  
+
     def book_comment_params
      params.require(:book_comment).permit(:comment)
     end
