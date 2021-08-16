17c691cda617a491ca6351be9b1d7e88fa34647d
yutakiguchi <y--k080816@docomo.ne.jp>
2021-08-16 02:42:08 +0000

    前日比

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,6 +4,8 @@ class Book < ApplicationRecord
 	has_many :favorite_users,through: :favorites,source: :user
 	has_many :book_comments,dependent: :destroy
 	is_impressionable counter_cache: true
+	scope :created_today, ->{ where(created_at: Time.zone.now.all_day) }
+	scope :created_yesterday, ->{ where(created_at: 1.day.ago.all_day) }
 
 	validates :title, presence: true
 	validates :body, presence: true, length: {maximum: 200}
